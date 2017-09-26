e09f7ff6f22564850254766831ac30438ebd326f
Riaan Van Onselen <riaanvo@yahoo.com.sg>
2017-09-26 17:19:23 +1000

    … should have been part of the previous commit

--- Decision_Tree_Builder_ID3/src/com/riaanvo/DataParser.java
+++ Decision_Tree_Builder_ID3/src/com/riaanvo/DataParser.java
@@ -5,40 +5,70 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class DataParser {
-    private Category classifications;
-    private ArrayList<Category> dataStructure = new ArrayList<Category>();
-    private ArrayList<DataElement> tainingData;
+    private String[] attibutes;
+    private LinkedList<LinkedList<String>> attibutesUniqueValues;
+    private String[][] traininngDataSet;
 
     public DataParser(String trainDataFilePath){
         //Extract training data file into a string
+        System.out.println("Loading training data set");
+
         String trainingCSVText = extractFileContents(trainDataFilePath);
+
+        System.out.println("Extracting data");
+
+
         String[] rows = trainingCSVText.split("\n");
-        //Define the structure of the csv file
-        buildDataStructure(rows[0]);
+
+        //Split ito columns
+        attibutes = rows[0].split(",");
+
+        //Construct data set
+        int numRows = rows.length - 1;
+        int numCols = attibutes.length;
+        traininngDataSet = new String[numRows][numCols];
+        for(int i = 1; i < rows.length; i++){
+            traininngDataSet[i - 1] = rows[i].split(",");
+        }
+
+        //Getting unique values
+        attibutesUniqueValues = new LinkedList<LinkedList<String>>();
+        for(int i = 0; i < numCols; i++){
+            String s = attibutes[i] + ": ";
+            LinkedList<String> uniqueValues = new LinkedList<String>();
+
+            for(int r = 0; r < numRows; r ++){
+                String value = traininngDataSet[r][i];
+                if(!uniqueValues.contains(value)){
+                    s += value + ", ";
+                    uniqueValues.add(value);
+                }
+            }
+            System.out.println(s);
+        }
 
 
-
-
+        printDataArray();
     }
 
-    private void buildDataStructure(String categoriesRow){
-        //Check if the row is the header row of the csv file
-        if(categoriesRow.contains("#")){
-            //categoriesRow = categoriesRow.replaceFirst("# ", "");
+    private void printDataArray(){
+        //Print out the data
+        String s = "\n\n\nData output\n";
 
-            //Split the fields into columns
-            String[] categories = categoriesRow.split(",");
-
-            //Set up the classification values structure;
-            classifications = new Category("class", -1);
-
-            //Assuming that the first column is the classification, start from one over and build the data structure.
-            for (int i = 1; i < categories.length; i++) {
-                dataStructure.add(new Category(categories[i], i - 1));
-            }
-        } else {
-            System.out.println("First row does not contain file headers!!!");
+        for(String a : attibutes){
+            s += a + ", ";
         }
+        s += "\n";
+
+        for(int r = 0; r < 10; r++){
+            for(int c = 0; c < attibutes.length; c++){
+                s += traininngDataSet[r][c] + ", ";
+            }
+            s += "\n";
+        }
+
+
+        System.out.println(s);
     }
 
     /**
@@ -79,56 +109,30 @@ public class DataParser {
         return output;
     }
 
-
-    private class Category{
-        private String fieldName;
-        private int categoryIndex;
-        private ArrayList<String> values;
-
-        public Category(String fieldName, int categoryIndex){
-            this.fieldName = fieldName;
-            this.categoryIndex = categoryIndex;
-            values = new ArrayList<String>();
-        }
-
-        public void tryAddValue(String value){
-            for(int i = 0; i < value.length(); i ++){
-                if(values.get(i).equals(value)){
-                    return;
-                }
-            }
-            values.add(value);
-        }
-
-        public int getValueIndex(String value){
-            for(int i = 0; i < value.length(); i ++){
-                if(values.get(i).equals(value)){
-                    return i;
-                }
-            }
-            return -1;
-        }
-
-        public int numDistinctValues(){
-            return values.size();
-        }
-
-        public String getFieldName(){
-            return fieldName;
-        }
-
-        public int getCategoryIndex(){
-            return categoryIndex;
-        }
-    }
-
     private class DataElement{
-        private int classification;
-        private int[] columnValues;
+        ArrayList<String> values;
 
-        public DataElement(int classification, int[] columnValues){
-            this.classification = classification;
-            this.columnValues = columnValues;
+        public DataElement(String[] values){
+            this.values = new ArrayList<>();
+            for(String value: values){
+                this.values.add(value);
+            }
+        }
+
+        public boolean matchesValue(int column, String value){
+            return values.get(column).equals(value);
+        }
+
+        public String toString(){
+            int numValues = values.size();
+            String s = "";
+            for(int i = 0; i < numValues; i++){
+                s += values.get(i);
+                if(i != numValues - 1) {
+                    s += ", ";
+                }
+            }
+            return s;
         }
     }
 }
